8690a9e58a97e0af5cd69e3484c807ab64aaf661
Joaquin Casares <joaquin@datastax.com>
2013-09-10 16:49:04 -0500

    Assign parameters correctly

--- wifileds/limitlessled/__init__.py
+++ wifileds/limitlessled/__init__.py
@@ -2,4 +2,4 @@ from . import bridge
 from . import effects
 
 def connect(address='192.168.1.100', port=50000, protocol='udp'):
-    return bridge.Bridge(address, port, protocol)
+    return bridge.Bridge(address=address, port=port, protocol=protocol)
